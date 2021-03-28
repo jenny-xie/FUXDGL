2fe979433211c5eb268c1e20cfd9b7393105ade0
Josh Fu <fu.joshua2001@gmail.com>
2021-03-28 11:16:25 -0400

    redirect after upload

--- backend/server.py
+++ backend/server.py
@@ -2,7 +2,7 @@ import time
 import json
 from gridfs import GridFS
 from pymongo import MongoClient
-from flask import Flask, make_response
+from flask import Flask, make_response, flash
 from flask import request, render_template, send_file, redirect, url_for
 from Parser import splice
 import os
@@ -20,19 +20,11 @@ grid_fs = GridFS(db)
 def uploadvid():
     #local copy
     video_file = request.files['video']
-    
-    if video_file.filename != '':
-        video_file.save(os.path.join(server.config['UPLOAD_PATH'],'recording.mp4'))
-    
+    if request.method == 'POST': 
+        if video_file.filename != '':
+            video_file.save(os.path.join(server.config['UPLOAD_PATH'],'recording.mp4'))
+        
 
-    # with grid_fs.new_file(filename='recording.mp4') as fp:
-    #     fp.write(request.data)
-    #     file_id = fp._id
-
-    # if grid_fs.find_one(file_id) is not None:
-    #     return json.dumps({'status': 'File saved successfully'}), 200
-    # else:
-    #     return json.dumps({'status': 'Error occurred while saving file.'}), 500
 
 # @server.route('/uploadfiles/', methods=['POST'])
 # def uploadFiles():
@@ -45,17 +37,6 @@ def uploadvid():
 #     if transcript_file.filename != '':
 #         transcript_file.save(os.path.join(server.config['UPLOAD_PATH'],'transcript.vtt'))
 
-    #possibly needs redirect to localhost3000:home
-
-    # with grid_fs.new_file(filename='transcript.vtt') as fp:
-    #     fp.write(request.data)
-    #     file_id = fp._id
-
-    # if grid_fs.find_one(file_id) is not None:
-    #     return json.dumps({'status': 'File saved successfully'}), 200
-    # else:
-    #     return json.dumps({'status': 'Error occurred while saving file.'}), 500
-
 @server.route('/splice/', methods=['POST'])
 def parse():
     fields = request.get_json().get('fields')
@@ -67,18 +48,21 @@ def parse():
 @server.route('/', methods=['GET', 'POST'])
 def uploadFiles():
     #local copy
-    video_file = request.files['video']
-    if video_file.filename != '':
-        video_file.save(os.path.join(server.config['UPLOAD_PATH'],'recording.mp4'))
+    if request.method == 'POST': 
+        video_file = request.files['video']
+        if video_file.filename != '':
+            video_file.save(os.path.join(server.config['UPLOAD_PATH'],'recording.mp4'))
 
-    transcript_file = request.files['transcript']
-    if transcript_file.filename != '':
-        transcript_file.save(os.path.join(server.config['UPLOAD_PATH'],'transcript.vtt'))
-    return redirect('/')
+        transcript_file = request.files['transcript']
+        if transcript_file.filename != '':
+            transcript_file.save(os.path.join(server.config['UPLOAD_PATH'],'transcript.vtt'))
+    return redirect(redirect_url())
 
-@server.route('/time')
-def get_current_time():
-    return {'time':time.time()}
+@server.route('/redirect')
+def redirect_url(default='index'):
+    return request.args.get('next') or \
+           request.referrer or \
+           url_for(default)
 
 @server.route('/test')
 def test():
