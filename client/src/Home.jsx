9badea47cff0b5778d17692c81a4549777d3d7a8
deguzmanc <deguzmanc@ufl.edu>
2021-03-28 02:54:26 -0400

    linked backend

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from './components/Header'
 import InputForm from './components/InputForm'
 
-
 export default function Homepage() {
-
+    const [time, setTime] = useState(0);
+    useEffect(() => {
+        fetch('/time').then(res => res.json()).then(data => {
+            setTime(data.time);
+        })
+    },[])
+    
     return (
         <>
             <Header></Header>
-            <h1>Homepage</h1>
+            <h1>Time: {time}</h1>
             <InputForm></InputForm>
         </>
     )
