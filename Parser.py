163ba50dfb50f2f24572e75b15a48dcb655a28d4
fu-jo <56369799+fu-jo@users.noreply.github.com>
2021-03-27 23:08:27 -0400

    added get_times function
    returns list of tuples with timestamps based on start and stop words

--- Parser.py
+++ Parser.py
@@ -2,22 +2,40 @@ import nltk
 from bs4 import BeautifulSoup as bs
 import requests
 
-transcript = ''
-# download webpage
-# url = 'https://ufl.zoom.us/rec/play/LzMDLeJHv2Jid0cU37Nr39Q6gwX7NKfrF5uCd9fgxANJIA7olzwLxHZVihpdjmRTfnfs8bCvbjyVDMJc.3B9SJWh-A5VrRSbm'
-# r = requests.get(url, allow_redirects=True)
-# open('transcript.html', 'wb').write(r.content)
-transcript += open('transcript.html', 'r').read()
+class Parser:
+    def __init__(self, start: str, stop: str):
+        self.start = start
+        self.stop = stop
+        self.raw = ''
+        # download webpage
+        # url = 'https://ufl.zoom.us/rec/play/LzMDLeJHv2Jid0cU37Nr39Q6gwX7NKfrF5uCd9fgxANJIA7olzwLxHZVihpdjmRTfnfs8bCvbjyVDMJc.3B9SJWh-A5VrRSbm'
+        # r = requests.get(url, allow_redirects=True)
+        # open('transcript.html', 'wb').write(r.content)
+        self.raw += open('transcript.html', 'r').read()
+        self.transcript = []
+        self.speech = []
+        self.times = []
 
+    def create_transcript(self, raw: str):
+        soup = bs(raw, 'html.parser')
+        transcript_list = soup.find_all('ul', {'class': 'transcript-list'})[0]
+        for item in transcript_list.contents:
+            a = item['aria-label'].split(',')[2:]
+            speech = ''.join(a)
+            timeline = item.find_all('div', {'class': 'timeline'})[0]
+            time = timeline.find_all('span', {'class': 'time'})[0].string
+            self.speech.append(speech)
+            self.times.append(time)
+            self.transcript.append((time, speech))
 
-def createTranscript(raw: str):
-    soup = bs(transcript, 'html.parser')
-    transcript_list = soup.find_all('ul', {'class': 'transcript-list'})[0]
-    for item in transcript_list.contents:
-        a = item['aria-label'].split(',')[2:]
-        speech = ''.join(a)
-        print(speech)
-        timeline = item.find_all('div', {'class': 'timeline'})[0]
-        time = timeline.find_all('span', {'class': 'time'})[0].string
-        print(time)
+    def get_times(self, transcript: list):
+        times = []
+        start_time = ''
+        for speech in self.transcript:
+            if self.start in speech[1]:
+                start_time = speech[0]
+            if self.end in speech[1]:
+                times.append((start_time, speech[0]))
+        return times
+
 
