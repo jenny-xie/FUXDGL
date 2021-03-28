635e9addd6e0f5b5857c94ed6952089508fae01a
Josh Fu <fu.joshua2001@gmail.com>
2021-03-28 04:17:13 -0400

    merged app and server

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,12 @@
 import time
+from pymongo import MongoClient
 from flask import Flask
 
 server = Flask(__name__)
+
+client = MongoClient('mongodb+srv://user:user123@cluster0.rjmqv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+db = client['database']
+col = db['transcripts']
 
 @server.route('/')
 def index():
@@ -10,6 +15,12 @@ def index():
 @server.route('/time')
 def get_current_time():
     return {'time':time.time()}
+
+@server.route('/test')
+def test():
+    mydict = { "name": "John", "address": "Highway 37" }
+    db.col.insert_one(mydict)
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     server.run()
