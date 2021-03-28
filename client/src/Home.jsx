b5574d5456a5efffeb9033369dd7f1438e8c4a20
deguzmanc <deguzmanc@ufl.edu>
2021-03-28 01:03:13 -0400

    fileUpload

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,27 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
-import { Form, Button } from 'react-bootstrap'
+import InputForm from './components/InputForm'
+
 
 export default function Homepage() {
-    const formSubmit = () => {
-        console.log('submit');
-    }
 
     return (
         <>
             <Header></Header>
             <h1>Homepage</h1>
-            <Form onSubmit={formSubmit}>
-                <Form.Group>
-                    <Form.Label>Transcript</Form.Label>
-                    <Form.Control type="file" />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Video</Form.Label>
-                    <Form.Control type="file" />
-                </Form.Group>
-                <Button type='submit'>Submit</Button>
-            </Form>
+            <InputForm></InputForm>
         </>
     )
 }
